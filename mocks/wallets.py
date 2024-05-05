31d2f42a748cec0e2c4f5aa5811442210df4e3d1
Nahuel <nbulian@gmail.com>
2024-05-05 19:24:56 -0300

    Hex to upper case

--- mocks/wallets.py
+++ mocks/wallets.py
@@ -1,17 +1,17 @@
 def get():
     wallets = {
         "my_wallet": {
-            "private_key_hex": "a312c062fef38d435716c56441491f1abe04b3106de357fe788a99086ee22c34",
+            "private_key_hex": "A312C062FEF38D435716C56441491F1ABE04B3106DE357FE788A99086EE22C34",
             "private_key_wif": "5K474waNvay5cvtYWaoVzE3RBkFZa4fm1445nE5YTw8FMv1e2xW",
             "public_key": "040C8786D25BA15E41564DD1811F990579B7F9E0D9463C4E1F34DEB7C88676E6A29AEC73379F377A0F73EC9BAF038D16F900CDCCC2D0EC1624AB8BBFF45A86E35A",
-            "public_key_hash": "a2e5b58d4dcd4c68454c1f42370513cd2bc6d42d",
+            "public_key_hash": "A2E5B58D4DCD4C68454C1F42370513CD2BC6D42D",
             "public_address": "1CteNXz1AFPi2MBcaEdeJH84rZA7XSCAjP",
         },
         "friend_wallet": {
-            "private_key_hex": "4619d4b57878d83c1a01d81b80ae7d9d9b4a8b3f95f10431b1340f969a6cf22f",
+            "private_key_hex": "4619D4B57878D83C1A01D81B80AE7D9D9B4A8B3F95F10431B1340F969A6CF22F",
             "private_key_wif": "5JMADRPeew9L3SoUYiDRF6LfUmgUskDuT1qMu9xdhgUPwUyesLL",
             "public_key": "04517CB662F6192A8B2B914A0B715C1AF76610B4CEAEE8885111AFD56CCC6735F4A1C2A2D6103C8ED01EAA4AFD946609539B0FAC6B591DB084F33AC8633B403F92",
-            "public_key_hash": "192e5ff5df3dc200fe3c2c98ce8e2fbdcae09a40",
+            "public_key_hash": "192E5FF5DF3DC200FE3C2C98CE8E2FBDCAE09A40",
             "public_address": "1NGfp5e3MK1oQcgLATj2hC1fkDEbNUupgS",
         }}
 
